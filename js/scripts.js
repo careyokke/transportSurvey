0e5322e6168d9c6a9260ebb453193f254465e8e8
Anna Kuznetsova and Michael Dunlap <anna+michael@>
2017-06-06 10:10:38 -0700

    add another checkbox group

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,12 @@ $(document).ready(function(){
       var workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
     });
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
     $("#work-responses").show();
+    $("#fun-responses").show();
     $('#transportation_survey').hide();
   });
 });
